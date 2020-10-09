79850bc1c4dc3a521b2d66356508882dd2dc4121
Julien <julien.vignoud@epfl.ch>
2020-10-09 19:32:41 +0200

    Test WS1/WS2/WS3

--- tests/feature_extraction.py
+++ tests/feature_extraction.py
@@ -31,14 +31,13 @@ class Features(unittest.TestCase):
         #All the activity at the same hour
         Lw = 10
         T = np.arange(Lw) * 3600 * 24
-        self.assertAlmostEqual(PDH(Lw, T), np.log2(24) * Lw, 2) #2 decimals accuracy
+        self.assertAlmostEqual(PDH(Lw, T), np.log2(24) * Lw, 2) 
 
     # PWD bounded in [0, log(7) * L_w]
     def test_PWD(self):  
         # Particular sid
         sid, T, Lw = getStudentTimeStamps(self.video_df, 9749)
-        self.assertAlmostEqual(PWD(Lw, T), 7.51, 2) #2 decimals accuracy
-
+        self.assertAlmostEqual(PWD(Lw, T), 7.51, 2)
         #Lower bound
         T = []
         Lw = 1
@@ -48,7 +47,30 @@ class Features(unittest.TestCase):
         #All the activity on the same day
         Lw = 10
         T = np.arange(Lw) * 3600 * 24 * 7
-        self.assertAlmostEqual(PWD(Lw, T), np.log2(7) * Lw, 2) #2 decimals accuracy
+        self.assertAlmostEqual(PWD(Lw, T), np.log2(7) * Lw, 2)
+
+    def test_WS1(self):
+        sid, T, Lw = getStudentTimeStamps(self.video_df, 10118)
+        self.assertAlmostEqual(WS1(Lw, T), 0.57, 1)
+
+        sid, T, Lw = getStudentTimeStamps(self.video_df, 46587)
+        self.assertAlmostEqual(WS1(Lw, T), 0.12, 2)
+
+    def test_WS2(self):
+        sid, T, Lw = getStudentTimeStamps(self.video_df, 10118)
+        self.assertAlmostEqual(WS2(Lw, T), 0.49, 2)
+
+        sid, T, Lw = getStudentTimeStamps(self.video_df, 46587)
+        self.assertAlmostEqual(WS2(Lw, T), 0.19, 2)
+
+    def test_WS3(self):
+        sid, T, Lw = getStudentTimeStamps(self.video_df, 10118)
+        self.assertAlmostEqual(WS3(Lw, T), 0.50, 2)
+
+        sid, T, Lw = getStudentTimeStamps(self.video_df, 46587)
+        self.assertAlmostEqual(WS3(Lw, T), 0.19, 0.18)
+
+
 
 if __name__ == '__main__':
     unittest.main()
