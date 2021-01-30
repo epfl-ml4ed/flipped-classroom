Feature set computation routine fixed

course/course.py
@@ -62,19 +62,23 @@ class Course():
     def label(self, labels=None, week_thr=None):
         assert self.clickstream_grade is not None and self.grade_thr is not None and self.grade_max is not None
 
-        if labels is None or 'pass-fail' in labels:
-            self.clickstream_grade['pass-fail'] = self.clickstream_grade['grade'].apply(lambda x: 1 if x < self.grade_thr * self.grade_max else 0)
+        if labels is None or 'label-grade' in labels:
+            self.clickstream_grade['label-grade'] = self.clickstream_grade['grade']
+            logging.info('assigned grades')
+
+        if labels is None or 'label-pass-fail' in labels:
+            self.clickstream_grade['label-pass-fail'] = self.clickstream_grade['grade'].apply(lambda x: 1 if x < self.grade_thr * self.grade_max else 0)
             logging.info('assigned {} pass and {} fail'.format(self.clickstream_grade['pass-fail'].to_list().count(0), self.clickstream_grade['pass-fail'].to_list().count(1), 'drop'))
 
-        if labels is None or 'dropout' in labels:
+        if labels is None or 'label-dropout' in labels:
             week_thr = week_thr if week_thr is not None else self.weeks
             max_week = self.get_clickstream_video().groupby(by='user_id')['week'].max()
-            self.clickstream_grade['dropout'] = np.array([(1 if row['user_id'] in max_week and max_week[row['user_id']] < week_thr - 1 else 0) for index, row in self.clickstream_grade.iterrows()])
+            self.clickstream_grade['label-dropout'] = np.array([(1 if row['user_id'] in max_week and max_week[row['user_id']] < week_thr - 1 else 0) for index, row in self.clickstream_grade.iterrows()])
             logging.info('assigned {} no-drop and {} drop'.format(self.clickstream_grade['dropout'].to_list().count(0), self.clickstream_grade['dropout'].to_list().count(1), 'drop'))
 
-        if labels is None or 'stopout' in labels:
+        if labels is None or 'label-stopout' in labels:
             max_week = self.get_clickstream_video().groupby(by='user_id')['week'].max()
-            self.clickstream_grade['stopout'] = np.array([max_week[row['user_id']] for index, row in self.clickstream_grade.iterrows()])
+            self.clickstream_grade['label-stopout'] = np.array([max_week[row['user_id']] for index, row in self.clickstream_grade.iterrows()])
             logging.info('assigned stopout')
 
     def get_weeks(self):

extractor/extractor.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from tqdm import tqdm
+import pandas as pd
 import numpy as np
-import pickle
+import logging
 import json
 import time
 import os
@@ -32,46 +33,60 @@ class Extractor():
         assert self.settings is not None
         return self.settings
 
+    def save(self, course, settings):
+        assert self.feature_values is not None and settings['filepath'].endswith('/')
+
+        if not os.path.exists(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id)):
+            os.makedirs(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id))
+
+        # Save the feature values
+        np.savetxt(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id, 'feature_values.csv'), self.feature_values[1], delimiter=',')
+        # Save the feature labels
+        self.feature_values[0].to_csv(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id, 'feature_labels.csv'), index=False)
+        # Save the current settings
+        with open(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id, 'settings.txt'), 'w') as file:
+            file.write(json.dumps({**settings, **{'course_id': course.course_id, 'type': course.type, 'platform': course.platform}}))
+
+        logging.info('saved features {} of shape {}'.format(self.name, self.feature_values[1].shape))
+
+    def load(self, settings):
+        assert os.path.join(settings['feature_set']) is not None
+        feature_values = np.genfromtxt(os.path.join(settings['workdir'], 'feature', settings['feature_set'], 'feature_values.csv'), delimiter=',')
+        feature_labels = pd.read_csv(os.path.join(settings['workdir'], 'feature', settings['feature_set'], 'feature_labels.csv'))
+        self.feature_values = (feature_labels, feature_values)
+        self.settings = json.load(open(os.path.join(settings['workdir'], 'feature', settings['feature_set'], 'settings.txt'), 'rb'))
+
     def get_features_values(self):
         assert self.feature_values is not None
         return self.feature_values
 
-    def save(self, course, settings):
-        assert self.feature_values is not None and settings['filepath'].endswith('/')
-        if not os.path.exists(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id)):
-            os.makedirs(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id))
-        with open(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id, 'feature_values.txt'), 'wb') as file:
-            pickle.dump(self.feature_values[1], file)
-        with open(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id, 'user_ids.txt'), 'wb') as file:
-            pickle.dump(self.feature_values[0], file)
-        with open(os.path.join(settings['filepath'], self.time + '-' + self.name + '-' + course.course_id, 'settings.txt'), 'w') as file:
-            file.write(json.dumps({**settings, **{'course_id': course.course_id, 'type': course.type, 'platform': course.platform}}))
-
-    def load(self, settings):
-        assert os.path.join(settings['feature_set']) is not None
-        self.feature_values = (pickle.load(open(os.path.join(settings['workdir'], 'feature', settings['feature_set'], 'user_ids.txt'), 'rb')), pickle.load(open(os.path.join(settings['workdir'], 'feature', settings['feature_set'], 'feature_values.txt'), 'rb')))
-        self.settings = json.load(open(os.path.join(settings['workdir'], 'feature', settings['feature_set'], 'settings.txt'), 'rb'))
-
     def extract_features(self, data, settings):
-        pass
+        return None
 
-    def load_features(self, course, settings, timeframe='week'):
-        clickstream = course.get_clickstream()
-        if timeframe == 'week':
-            weeks = np.arange(clickstream['week'].max() - 1)
-            users = clickstream['user_id'].unique()
-            user_ids = []
-            features_values = []
-            for u, user_id in tqdm(enumerate(users)):
-                user_ids.append(user_id)
-                user_feature_values = []
-                data = clickstream[clickstream['user_id'] == user_id].sort_values(by='date')
-                for w, week in enumerate(weeks):
-                    user_feature_values.append(self.extract_features(data, {**settings, **{'timeframe': timeframe, 'week': week, 'course': course}}))
-                features_values.append(user_feature_values)
-            self.feature_values = (user_ids, np.array(features_values))
-            self.settings = settings
-            self.save(course, settings)
+    def extract_features_bunch(self, course, settings):
+        assert 'timeframe' in settings
+
+        data = course.get_clickstream()
+        label = course.get_clickstream_grade()
+        filter_label_cols = [col for col in label if col.startswith('label_')]
+
+        weeks = np.arange(data['week'].max() - 1)
+        users = data['user_id'].unique()
+
+        features_values = []
+        features_labels = []
+        for u, user_id in tqdm(enumerate(users)):
+            user_feature_values = []
+            for w, week in enumerate(weeks):
+                user_feature_values.append(self.extract_features(data[data['user_id'] == user_id], {**settings, **{'week': week, 'course': course}}))
+            features_values.append(user_feature_values)
+            features_labels.append([u] + label[label['user_id'] == user_id][filter_label_cols].head(1).tolist())
+
+        self.feature_values = (pd.DataFrame(features_labels, columns=['user_index'] + filter_label_cols), np.array(features_values))
+        self.settings = settings
+        logging.info('computed features {} of shape {}'.format(self.name, self.feature_values[1].shape))
+
+        self.save(course, settings)
 
     def __len__(self):
         assert self.features is not None