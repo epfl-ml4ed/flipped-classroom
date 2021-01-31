2523750a059a240449dd2f386aa8ca0115718bde
mirkomarras <m.marras19@gmail.com>
2021-01-31 18:11:45 +0100

    Ensemble added and integrated in sbatch prediction linear algebra

--- extractor/extractor_loader.py
+++ extractor/extractor_loader.py
@@ -13,6 +13,43 @@ class ExtractorLoader(Extractor):
     def __init__(self):
         super().__init__('extractor_loader')
 
+    def load(self, settings):
+        assert 'feature_set' in settings
+        if isinstance(settings['feature_set'], list):
+            return self.load_ensemble(settings)
+        return super().load(settings)
+
+    def load_ensemble(self, settings):
+        assert 'feature_set' in settings and isinstance(settings['feature_set'], list)
+
+        self.settings = {}
+        feature_labels = None
+        feature_values = None
+        course_id, course_type, course_platform = None, None, None
+        for fs in settings['feature_set']:
+            # Merge features
+            feature_values_tmp = np.load(os.path.join(settings['workdir'], 'feature', fs, 'feature_values.npz'))['feature_values']
+            if feature_values is None:
+                feature_values = feature_values_tmp
+            else:
+                feature_values = np.append(feature_values, feature_values_tmp, axis=2)
+            # Check same labels
+            feature_labels_tmp = pd.read_csv(os.path.join(settings['workdir'], 'feature', fs, 'feature_labels.csv'))
+            assert feature_labels == None or feature_labels.equals(feature_labels_tmp)
+            feature_labels = feature_labels_tmp
+            # Check same course and other settings
+            settings = json.load(open(os.path.join(settings['workdir'], 'feature', settings['feature_set'], 'settings.txt'), 'rb'))
+            assert course_id == None or course_id == settings['course_id']
+            course_id = settings['course_id']
+            assert course_type == None or course_type == settings['type']
+            course_type = settings['type']
+            assert course_platform == None or course_platform == settings['platform']
+            course_platform = settings['platform']
+
+        # Prepare settings and feature values pair as for any other feature set
+        self.settings = {'course_id': course_id, 'type': course_type, 'platform': course_platform}
+        self.feature_values = (feature_labels, feature_values)
+
     def save(self, course, settings):
         raise NotImplementedError()
 
--- sbatch/sbatch_linear_algebra_predictor.py
+++ sbatch/sbatch_linear_algebra_predictor.py
@@ -9,6 +9,33 @@ from routine.train_predictor import main
 
 logging.getLogger().setLevel(logging.INFO)
 
+def train_individual(timeframes, course_ids, predictors, base_settings):
+    for timeframe in timeframes:
+        for course_id in course_ids:
+            feature_sets = [fs for fs in os.listdir('../data/result/edm21/feature') if timeframe in fs and course_id in fs]
+            for feature_set in feature_sets:
+                feature_labels = pd.read_csv(os.path.join('../data/result/edm21/feature', feature_set, 'feature_labels.csv'))
+                filter_label_cols = [col for col in feature_labels.columns.tolist() if col.startswith('label-')]
+                filter_label_classes = [1 for _ in filter_label_cols]
+                filter_label_types = [('classification' if not 'grade' in f and not 'stopout' in f else 'regression') for f in filter_label_cols]
+                for label_col, label_class, label_type in zip(filter_label_cols, filter_label_classes, filter_label_types):
+                    for predictor in predictors:
+                        logging.info('{} {} {} {} {}'.format(timeframe, course_id, feature_set, (label_col, label_class, label_type), predictor))
+                        main({**base_settings, **{'timeframe': timeframe, 'course_id': course_id, 'model': predictor, 'target': label_col, 'target_type': label_type, 'classes': label_class, 'feature_set': feature_set, 'workdir': '../data/result/edm21'}})
+
+def train_ensemble(timeframes, course_ids, predictors, base_settings):
+    for timeframe in timeframes:
+        for course_id in course_ids:
+            feature_sets = [fs for fs in os.listdir('../data/result/edm21/feature') if timeframe in fs and course_id in fs]
+            feature_labels = pd.read_csv(os.path.join('../data/result/edm21/feature', feature_sets[0], 'feature_labels.csv'))
+            filter_label_cols = [col for col in feature_labels.columns.tolist() if col.startswith('label-')]
+            filter_label_classes = [1 for _ in filter_label_cols]
+            filter_label_types = [('classification' if not 'grade' in f and not 'stopout' in f else 'regression') for f in filter_label_cols]
+            for label_col, label_class, label_type in zip(filter_label_cols, filter_label_classes, filter_label_types):
+                for predictor in predictors:
+                    logging.info('{} {} {} {} {}'.format(timeframe, course_id, 'ensemble', (label_col, label_class, label_type), predictor))
+                    main({**base_settings, **{'timeframe': timeframe, 'course_id': course_id, 'model': predictor, 'target': label_col, 'target_type': label_type, 'classes': label_class, 'feature_set': feature_sets, 'workdir': '../data/result/edm21'}})
+
 if __name__ == "__main__":
     grid = {
         'mlp': {'activation': ('identity', 'logistic', 'tanh', 'relu'), 'solver': ('lbfgs', 'sgd', 'adam'), 'hidden_layer_sizes': [(8,), (16, 8), (32, 16, 8)]},
@@ -22,18 +49,9 @@ if __name__ == "__main__":
     }
 
     timeframes = ['eq-week', 'lq-week']
-    course_ids = ['epfl-algebrelineaire-2018', 'epfl-algebrelineaire-2019']
+    course_ids = ['epfl-algebrelineaire']
     predictors = ['predictor.' + fs.split('.')[0] + '.' + fs.split('.')[0].replace('_', '.').title().replace('.', '') for fs in os.listdir('../predictor') if os.path.isfile(os.path.join('../predictor', fs)) and not 'pycache' in fs and not 'predictor' in fs]
 
-    for timeframe in timeframes:
-        for course_id in course_ids:
-            feature_sets = [fs for fs in os.listdir('../data/result/edm21/feature') if timeframe in fs and course_id in fs]
-            for feature_set in feature_sets:
-                feature_labels = pd.read_csv(os.path.join('../data/result/edm21/feature', feature_set, 'feature_labels.csv'))
-                filter_label_cols = [col for col in feature_labels.columns.tolist() if col.startswith('label-')]
-                filter_label_classes = [1 for f in filter_label_cols]
-                filter_label_types = [('classification' if not 'grade' in f and not 'stopout' in f else 'regression') for f in filter_label_cols]
-                for label_col, label_class, label_type in zip(filter_label_cols, filter_label_classes, filter_label_types):
-                    for predictor in predictors:
-                        logging.info('{} {} {} {} {}'.format(timeframe, course_id, feature_set, (label_col, label_class, label_type), predictor))
-                        main({**base_settings, **{'timeframe': timeframe, 'course_id': course_id, 'model': predictor, 'target': label_col, 'target_type': label_type, 'classes': label_class, 'feature_set': feature_set, 'workdir': '../data/result/edm21'}})
+    train_individual(timeframes, course_ids, predictors, base_settings)
+
+    train_ensemble(timeframes, course_ids, predictors, base_settings)
